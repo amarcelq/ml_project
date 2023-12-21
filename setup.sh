9bd0823dec252b64f1d7d0fd986ebbec764cb01a
seba_mobile <>
2023-12-21 16:00:50 +0100

    venv

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 python3.11 -m venv venv
+#echo "Now activate venv with:"
 source ./venv/bin/activate
+#echo "Then install requirements with:"
 pip3.11 install -r requirements
 echo "Start Jupyter Lab with:"
-echo "  python3.11 -m jupyter-lab ."
+python3.11 -m jupyterlab .
