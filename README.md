4270c5ccf13e828d934dcd47b172d05e1cea9fe6
seba_mobile <>
2024-01-09 05:18:47 +0100

    .

---
+++ README.md
@@ -0,0 +1,5 @@
+# ML Projekt - Bat Orientation Calls
+
+## Ideads
+- data pipeline to reduce memory usage
+- Mit Matrix schauen, welche Klassen am meisten falsch predicted werden um vllt beim resampling gegensteuern zu können
---
+++ run.sh
@@ -0,0 +1,29 @@
+# check for aguments $1
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied\nUsage: $0 jupyter-notebook.ipynb [output-name]\n"
+  exit
+fi
+
+FILE_NAME=$1
+if [ -z "$2" ]
+then
+  OUTPUT_NAME=$(echo $1 | sed 's/.ipynb//')
+else
+  OUTPUT_NAME=$2
+fi
+
+jupyter nbconvert --to script $FILE_NAME --output  $OUTPUT_NAME
+
+grep -v '^get_ipython' $OUTPUT_NAME.py > tempfile && mv tempfile $OUTPUT_NAME.py
+nice -n -20 sh -c "python3 $OUTPUT_NAME.py &" & pid=$!
+
+wait $pid 
+echo "Process has ID $pid"
+exit
