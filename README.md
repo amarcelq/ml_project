800072a661f86546c4ee251b465510ca0b874a5b
seba_mobile <>
2024-01-09 05:16:29 +0100

    .

--- README.md
+++
@@ -1,5 +0,0 @@
-# ML Projekt - Bat Orientation Calls
-
-## Ideads
-- data pipeline to reduce memory usage
-- Mit Matrix schauen, welche Klassen am meisten falsch predicted werden um vllt beim resampling gegensteuern zu können
--- setup.sh
+++
@@ -1,7 +0,0 @@
-python3.11 -m venv venv
-#echo "Now activate venv with:"
-source ./venv/bin/activate
-#echo "Then install requirements with:"
-pip3.11 install -r requirements
-echo "Start Jupyter Lab with:"
-python3.11 -m jupyterlab .
