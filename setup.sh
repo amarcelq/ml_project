2dcdd82b501db6ba4c05f894f7ecf9172233f8d5
seba_mobile <>
2024-01-12 20:05:58 +0100

    ergebnisse

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-python3.9 -m venv venv_3_9
+python3.10 -m venv venv_3_10
 #echo "Now activate venv with:"
-source ./venv_3_9/bin/activate
+source ./venv_3_10/bin/activate
 #echo "Then install requirements with:"
-pip3.9 install -r requirements
+pip3.10 install -r requirements
 echo "Start Jupyter Lab with:"
-python3.9 -m jupyterlab .
+python3.10 -m jupyterlab .
